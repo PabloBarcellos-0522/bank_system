d3e2887e5bd8be198b319cac700b13ba072762f1
Pablo Barcellos Soares <pablobarcellossoares@gmail.com>
2024-03-03 16:50:06 -0300

    add User and Conta

---
+++ src/main/java/com/pablobarcellos/software_bancario/models/Conta.java
@@ -0,0 +1,79 @@
+package com.pablobarcellos.software_bancario.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Conta")
+public class Conta
+{
+    @Id
+    @Column(name = "numeroConta", unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer numeroConta;
+    
+    
+    @Column(name = "saldo", unique = true)
+    @NotNull
+    @NotEmpty
+    private Double saldo;
+
+
+    @Column(name = "extrato", unique = true)
+    @NotNull
+    @NotEmpty
+    @Size(max = 999)
+    private String extrato;
+
+    @ManyToOne
+    @JoinColumn(name = "User_ID")
+    private User user;
+
+    public Conta()
+    {}
+    
+
+
+
+
+    public Integer getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(Integer numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getExtrato() {
+        return extrato;
+    }
+
+    public void setExtrato(String extrato) {
+        this.extrato = extrato;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/pablobarcellos/software_bancario/models/User.java
@@ -0,0 +1,125 @@
+package com.pablobarcellos.software_bancario.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "User")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idUser", unique = true)
+    private Integer idUser;
+
+    @Column(name = "userName", nullable = false, unique = true)
+    @NotNull
+    @NotEmpty
+    @Size(min = 3, max = 15)
+    private String username;
+    
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(name = "password", nullable = false, unique = true)
+    @NotNull
+    @NotEmpty
+    @Size(min = 5, max = 10)
+    private String password;
+
+
+    @OneToMany(mappedBy = "user")
+    private List<Conta> contas = new ArrayList<>();
+
+
+    
+    public User()
+    {}
+
+    public User(Integer idUser,
+                @NotNull @NotEmpty @Size(min = 3, max = 15)String username,
+                @NotNull @NotEmpty @Size(min = 5, max = 10) String password)
+    {
+        this.idUser = idUser;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    // Getters and Setters
+    public Integer getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Integer idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    // HashCode and Equals
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((idUser == null) ? 0 : idUser.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (idUser == null) {
+            if (other.idUser != null)
+                return false;
+        } else if (!idUser.equals(other.idUser))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        return true;
+    }
+}
